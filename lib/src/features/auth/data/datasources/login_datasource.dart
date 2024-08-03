67776ec22858f094eaf1e8b2f80bd679ef8130b2
CevinWays <cevinways34@gmail.com>
2024-08-03 08:09:24 +0700

    add list ui

---
+++ lib/src/features/auth/data/datasources/login_datasource.dart
@@ -0,0 +1,12 @@
+
+
+abstract class LoginDataSource {
+  // Future<LoginModel> getLogin({
+  //   required String email,
+  //   required String password,
+  // });
+
+  // Future<LoginModel> getLoginWithGoogle();
+
+  Future<void> logout();
+}
---
+++ lib/src/features/auth/data/datasources/login_datasource_impl.dart
@@ -0,0 +1,10 @@
+import 'package:list_app/src/features/auth/data/datasources/login_datasource.dart';
+
+class LoginDatasourceImpl implements LoginDataSource {
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+  
+}
